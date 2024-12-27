3f64872989dc795b3439f9e8088a4c03a438f1de
Valentin <valentin2romero@gmail.com>
2024-12-27 00:55:59 -0300

    [ADD] app sale_order_report_format_custom

---
+++ sale_order_report_format_custom/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Sale Order Report Format Custom",
+    "version": "16.0.1.0.0",
+    "author": "Valentin Romero",
+    "summary": "Print sale order Custom",
+    "website": "",
+    "category": "Sale",
+    "license": "AGPL-3",
+    "depends": ["sale","account"],
+    "data": [
+        "report/report_saleorder_document.xml",
+        "report/account_reports.xml",
+    ],
+    "installable": True,
+}
